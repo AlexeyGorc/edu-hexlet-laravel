5d59f729e0edadf53d8bb285c4474bc62512948f
AlexeyGoncharenko <zomorc@gmail.com>
2024-04-15 11:47:43 +0200

    added layout

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,2 +1,13 @@
+@extends('layouts.app')
+
+@section('title', 'О блоге')
+
+@section('content')
 <h1>О блоге</h1>
 <p>Эксперименты с Laravel на Хекслете</p>
+
+@foreach ($team as $person)
+    <p>{{ $person['name'] }}</p>
+    <p>{{ $person['position'] }}</p>
+@endforeach
+@endsection
